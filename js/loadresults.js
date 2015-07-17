417e891a03f1114744e631399825b9c76957c3ab
zanagi <kintoki.douji@gmail.com>
2015-07-17 16:09:58 +0300

    measurements: fix loading logic, add debug function print(), tested that parsing works now

--- js/loadresults.js
+++ js/loadresults.js
@@ -1,5 +1,7 @@
 $(window).ready(function(){
-	loadResults("test");
+	var dataHandler = new DataHandler();
+	
+	loadResults("heaven");
 	
 	/**
 	 * Loads the measurements with the given name and stores them to be displayed on the HTML page
@@ -9,7 +11,7 @@ $(window).ready(function(){
 		var path = "measurements/" + name + ".txt";
 		
 		$.get(path, function(data){
-			parse(data);
+			parse(data, path);
 		}, "json")
 		.fail(function(a, b, c){
 			displayError("Invalid file: " + path);
@@ -19,17 +21,25 @@ $(window).ready(function(){
 	/**
 	 * Parses measurement data into usable class objects
 	 * @param data {Object} The JSON representation of the measurement data
+	 * @param path {String} The path of the data file
 	 */
-	function parse(data) {
+	function parse(data, path) {
 		for (var property in data) {
 		    if (data.hasOwnProperty(property)) {
-		        console.log(property); // Quadro K2000 etc
-		        console.log(data[property]); // Value 345
+		        var split = property.split("/"); 			// Machine/Card, i.e Qemu/Quadro K2000
+		        var values =  data[property].split("/"); 	// Values, i.e 345/346/347
+		        	
+		        if(split.length != 2){
+		        	displayError("Machine/Card format " + property + " not valid in: " + path);
+		        	return;
+		        }
+		        dataHandler.pushValues(split[0], split[1], values); // Machine, Card, Values
 		    }
 		}
 		
 		// Debug
 		// console.log(data);
+		dataHandler.print();
 	}
 
 	/**
@@ -42,51 +52,88 @@ $(window).ready(function(){
 		// TODO: DOM element change to display error?
 	}
 
-	/**
-	 * Class for graphics cards used in benchmarking
-	 * @param name {String} Name of the graphics card
-	 */
-	function Card(name) {
+	
+	function Machine(name) {
 		
-		this.getName = function(){
+		var pairs = []; // Array of DataPair instances
+		
+		this.getName = function() {
 			return name;
 		};
-	}
-
-	/**
-	 * Class for "graphics card"-"benchmark score" pairs
-	 * @param card {Card} The Card class instance
-	 * @param value {Number} The benchmark score value
-	 */
-	function DataPair(card, value) {
 		
-		this.hasCard = function(name) {
-			return (card.getName === name)
+		this.pushPair = function(card, values) {
+			pairs.push(new DataPair(card, values));
 		};
 		
-		this.getValue = function(){
-			return value;
+		/**
+		 * Debug function
+		 */
+		this.print = function() {
+			console.log(name + ": ");
+			for(var i = 0; i < pairs.length; i++){
+				pairs[i].print();
+			}
+		};
+	}
+	
+	/**
+	 * Class for "graphics card"-"benchmark score" pairs
+	 * @param card {String} The name of the graphics card
+	 * @param values {Array} The benchmark score values
+	 */
+	function DataPair(card, values) {
+		
+		this.hasCard = function(name) {
+			return (card === name)
+		};
+		
+		this.getValues = function(){
+			return values;
 		}
+		
+		this.print = function(){
+			console.log(card + ": " + values);
+		};
 	}
 
 	function DataHandler() {
 		
-		// Array of DataPair objects
-		var pairs = [];
+		var machines = []; // Array of machines
 		
-		this.getValue = function(name) {
-			for(var pair in pairs){
-				if(pair.hasCard(name)){
-					return pair.getValue();
+		this.pushValues = function(name, card, values){
+			var machineExists = false;
+			
+			for(var i = 0; i < machines.length; i++){
+				var m = machines[i];
+				if(m.getName() == name){
+					// If machine already exits just push the values into it
+					m.pushPair(card, values);
+					machineExists = true;
+					
+					// Break out of loop
+					break;
 				}
 			}
 			
-			// TODO: throw exception?
-			return null;
+			// No matching machine found: create a new one
+			if(!machineExists){
+				var machine = new Machine(name);
+				machine.pushPair(card, values);
+				machines.push(machine);
+			}
 		};
 		
 		this.constructInterface = function(){
 			// TODO: Create DOM objects to display data
+		};
+		
+		/**
+		 * Debug function
+		 */
+		this.print = function() {
+			for(var i = 0; i < machines.length; i++) {
+				machines[i].print();
+			}
 		};
 	}
 });
