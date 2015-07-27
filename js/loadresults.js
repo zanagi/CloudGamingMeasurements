5f1d46d9b17a4183d5efbde7bfed681911a29607
zanagi <kintoki.douji@gmail.com>
2015-07-27 09:25:23 +0300

    multi-gpu benchmark measurements

--- js/loadresults.js
+++ js/loadresults.js
@@ -28,19 +28,10 @@ function parse(data, path) {
 	        
 	        // Check if 1st part is formatted correctly
 	        if(split.length != 2){
-	        	displayError("Machine/Card format " + property + " not valid in: " + path);
+	        	displayError("Invalidly formatted data in: " + path)
 	        	return;
 	        }
 	        
-	        // Check that every value is a numeric value and convert them to floating numbers
-	        for(var i = 0; i < values.length; i++){
-	        	var float = parseFloat(values[i]);
-	        	if(isNaN(float)){
-	        		displayError("Non-numeric value(s) in: " + path);
-	        		return;
-	        	}
-	        	values[i] = float;
-	        }
 	        dataHandler.pushValues(split[1], split[0], values); // Card, Machine, Values
 	    }
 	}
@@ -51,25 +42,16 @@ function parse(data, path) {
 	dataHandler.print();
 }
 
-/**
- * Displays an error on the HTML page
- * @param msg {string} The message to be displayed to the user
- */
-function displayError(msg) {
-	alert("Error! " + msg);
-	
-	// TODO: DOM element change to display error?
-}
 
 /**
- * Class for "machines" used in benchmarking
- * @param name {String} the name of the machine
+ * Class for "graphics cards" used in benchmarking
+ * @param name {String} the name of the card
  */
 function Card(name) {
 	var pairs = []; // Array of DataPair instances
 	
 	/**
-	 * Returns the name of the machine
+	 * Returns the name of the card
 	 */
 	this.getName = function() {
 		return name;
@@ -153,9 +135,72 @@ function DataPair(machine, values) {
 	};
 }
 
+
+function MultiCard(name, machine, values) {
+	var split = name.split("&");
+	var cards = [];
+	var mappedValues = values.map(function(valueString){
+		var temp = valueString.split("&");
+		
+		if(temp.length != split.length) {
+			displayError("The number of cards and the number of values in a segment is not equal.");
+		}
+
+		return temp;
+	});
+	
+	for(var i = 0; i < split.length; i++){
+		var card = new Card(split[i]);
+		card.pushPair(machine, mappedValues.map(function(valueArray){
+			var float = parseFloat(valueArray[i]);
+			if(isNaN(float)){
+        		displayError("Non-numeric value(s) in multicard: " + split);
+        		return;
+        	}
+			return float;
+		}));
+		cards.push(card);
+	}
+	
+	this.getName = function() {
+		return name.replace("&", " & ");
+	};
+	
+	this.getMachine = function() {
+		return machine;
+	}
+	
+	this.getCards = function() {
+		return cards;
+	}
+	
+	this.getCardCount = function() {
+		return cards.length;
+	}
+	
+	this.getPairs = function() {
+		// All cards have the same 
+		return cards[0].getPairs();
+	};
+	
+	/**
+	 * Debug function
+	 */
+	this.print = function(){
+		console.log("-----------------");
+		console.log("Multicard: " + this.getName());
+		for(var i = 0; i < cards.length; i++){
+			cards[i].print();
+		}
+		console.log("-----------------");
+	};
+}
+
+
 function DataHandler() {
 	
 	var cards = []; // Array of graphics cards
+	var multiCards = []; // Array of card combinations
 	
 	/**
 	 * Push values into a machine
@@ -164,6 +209,22 @@ function DataHandler() {
 	 * @param values {Array} The measurement values
 	 */
 	this.pushValues = function(name, machine, values){
+		
+		// Check multi-gpu
+		if(name.indexOf("&") > -1){
+			multiCards.push(new MultiCard(name, machine, values));
+			return;
+		}
+
+		// Check that values are all numerical and parse them to floats
+		for(var i = 0; i < values.length; i++){
+			var float = parseFloat(values[i]);
+			if(isNaN(float)){
+        		displayError("Non-numeric value(s) in: " + path);
+        		return;
+        	}
+			values[i] = float;
+		}
 		var cardExists = false;
 		
 		for(var i = 0; i < cards.length; i++){
@@ -196,14 +257,110 @@ function DataHandler() {
 			createGraphs(card);
 		}
 		
+		createSelectionMulti(multiCards);
+		createGraphsMulti(multiCards);
+		
 		$(".select-block").click(function(){
 			// Toggle selection block transparency
 			$(this).toggleClass("inactive");
 			
 			// Show/Hide the graph
-			$("#" + $(this).data("graph")).toggleClass("hidden");
+			$("#" + jQueryString($(this).data("graph"))).toggleClass("hidden");
 		});
 	};
+	
+	function createSelectionMulti(multiCards) {
+		var name = multiCards[0].getName();
+		
+		// Create the selection area
+		var area = document.createElement("div");
+		$(area).addClass("select-area");
+		
+		// Create the area title and append it to area
+		var areaTitle = document.createElement("p");
+		var text = document.createTextNode(name);
+		areaTitle.appendChild(text);
+		$(areaTitle).addClass("center-text");
+		$(area).append(areaTitle);
+		
+		for(var i = 0; i < multiCards.length; i++){
+			var multiCard = multiCards[i];
+			// Initialize block
+			var block = document.createElement("div");
+			$(block).addClass("select-block"); // Start the blocks as inactive?
+			$(block).data("graph", withoutSpaces(name) + "-" + multiCard.getMachine());
+			
+			// Initialize text paragraph
+			var blockText = document.createElement("p");
+			var t = document.createTextNode(multiCard.getMachine());
+			blockText.appendChild(t);
+			$(blockText).addClass("center-text");
+			
+			$(block).append(blockText);
+			$(area).append(block);
+		}
+		
+		$("#select-multi").append(area);
+	}
+	
+	function createGraphsMulti(multiCards) {
+		var graphHeight = 200;
+		var cardsName = multiCards[0].getName();
+		
+		var rowTitle = document.createElement("h3");
+		var titleText = document.createTextNode(cardsName);
+		rowTitle.appendChild(titleText);
+		$(rowTitle).addClass("center-text");
+		$("#content").append(rowTitle);
+		
+		var row = document.createElement("div");
+		$(row).addClass("row");
+		
+		for(var i = 0; i < multiCards.length; i++){
+			var multiCard = multiCards[i];
+			var graphWidth = 120.0 / multiCard.getCardCount();
+			var maxValue = getMaxOfArray(multiCard.getPairs().map(function(p){
+				return p.getAverage();
+			}));
+			var machineName = multiCard.getMachine();
+			var areaId = withoutSpaces(cardsName) + "-" + machineName;
+			
+			
+			var graphsArea = document.createElement("div");
+			$(graphsArea).height(graphHeight + "px");
+			$(graphsArea).addClass("graph-area");
+			$(graphsArea).attr("id", areaId); // Add id for the selection onclick show/hide effect
+			
+			var cards = multiCard.getCards();			
+			for(var j = 0; j < cards.length; j++){
+				var average = cards[j].getPairs()[0].getAverage();
+				
+				// Create graph
+				var graph = document.createElement("div");
+				var gHeight = 0.9 * graphHeight * average / maxValue; // 0.9 coefficient to limit max-height
+				$(graph).height(gHeight + "px");
+				$(graph).width(graphWidth + "px");
+				$(graph).addClass("graph");
+				$(graph).css("margin-left", (graphWidth * j) + "px");
+				
+				// Initialize the element containing the average value
+				var valueTitle = document.createElement("h4");
+				var valueText = document.createTextNode(Math.round(average * 100) / 100); // Convert average score to a 2-decimal float
+				valueTitle.appendChild(valueText);
+				$(valueTitle).addClass("center-text graph-value");
+				$(graph).append(valueTitle);
+				
+				$(graphsArea).append(graph);
+			}
+			var machineTitle = document.createElement("h4");
+			var machineText = document.createTextNode(machineName);
+			machineTitle.appendChild(machineText);
+			$(machineTitle).addClass("center-text graph-title");
+			$(graphsArea).append(machineTitle);
+			$(row).append(graphsArea);
+		}
+		$("#content-multi").append(row);
+	}
 	
 	function createSelection(card) {
 		var name = card.getName();
@@ -247,6 +404,7 @@ function DataHandler() {
 	 */
 	function createGraphs(card) {
 		var graphHeight = 200;
+		var graphWidth = 120;
 		var cardName = card.getName();
 		var pairs = card.getPairs();
 		var maxValue = getMaxOfArray(pairs.map(function(p){
@@ -276,8 +434,9 @@ function DataHandler() {
 			
 			// Create graph
 			var graph = document.createElement("div");
-			var gHeight = 0.9 * graphHeight * average / maxValue; // 0.95 coefficient to limit max-height
+			var gHeight = 0.9 * graphHeight * average / maxValue; // 0.9 coefficient to limit max-height
 			$(graph).height(gHeight + "px");
+			$(graph).width(graphWidth + "px");
 			$(graph).addClass("graph");
 			
 			// Initialize the element containing the average value
@@ -293,7 +452,7 @@ function DataHandler() {
 			$(machineTitle).addClass("center-text graph-title")
 
 			$(graph).append(valueTitle);
-			$(graph).append(machineTitle);
+			$(graphsArea).append(machineTitle);
 			$(graphsArea).append(graph);
 			$(row).append(graphsArea);
 		}
@@ -307,13 +466,35 @@ function DataHandler() {
 		for(var i = 0; i < cards.length; i++) {
 			cards[i].print();
 		}
+		for(var i = 0; i < multiCards.length; i++){
+			multiCards[i].print();
+		}
 	};
 }
 
+/**
+ * Displays an error on the HTML page
+ * @param msg {string} The message to be displayed to the user
+ */
+function displayError(msg) {
+	alert("Error! " + msg);
+	
+	// TODO: DOM element change to display error?
+}
+
+/**
+ * 
+ * @param numArray
+ * @returns the maximum value of the given array
+ */
 function getMaxOfArray(numArray) {
 	return Math.max.apply(null, numArray);
 }
 
 function withoutSpaces(str) {
 	return str.replace(/\s+/g, '');
+}
+
+function jQueryString(str) {
+	return str.replace(/[!"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~]/g, "\\$&");
 }
